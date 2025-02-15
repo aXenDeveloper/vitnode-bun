2ebebadd4060878e9db0df348b85873946ee5aa1
aXenDeveloper <axendeveloper@gmail.com>
2025-02-15 14:26:40 +0100

    chore: Clean up

--- packages/vitnode/src/views/auth/sign-up/form/hooks/mutation-api.ts
+++ packages/vitnode/src/views/auth/sign-up/form/hooks/mutation-api.ts
@@ -1,14 +1,14 @@
 'use server';
 
-import { UsersTypes } from '@/api/core/users/routes';
-import { fetcher } from '@/utils/fetcher';
+// import { UsersTypes } from '@/api/core/users/routes';
+// import { fetcher } from '@/utils/fetcher';
 
 export const mutationApi = async () => {
-  await fetcher<UsersTypes>({
-    path: '/sign_up',
-    method: 'post',
-    input: {
-      json: values,
-    },
-  });
+  // await fetcher<UsersTypes>({
+  //   path: '/sign_up',
+  //   method: 'post',
+  //   input: {
+  //     json: values,
+  //   },
+  // });
 };
